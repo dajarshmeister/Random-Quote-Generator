e7d397ec7ed52f5f7baaa972e7c45d0bd836840d
Josh P <joshpalmeroc@gmail.com>
2019-08-15 19:00:38 -0700

    removed comments from treehouse

--- js/script.js
+++ js/script.js
@@ -3,18 +3,6 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
 // Creates a list of quotes with various information
 
 var quotes = [
@@ -51,33 +39,12 @@ var quotes = [
 
 console.log(quotes);
 
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Use the random number to `return` a random quote object from the `quotes` array.
-***/
-
 // Selects a random object from the quotes array
 
 function getRandomQuote () {
   var randNum = Math.floor(Math.random() * quotes.length);
   return quotes[randNum];
 }
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
 
 // Generates a random number to be used when updating background color of page
 
@@ -106,7 +73,6 @@ function printQuote () {
     message+= `<p class="tags">Tags: <i>${randomQoute.tags}</i></p>`;
   }
 
-
   document.getElementById('quote-box').innerHTML = message;
 }
 
@@ -114,14 +80,6 @@ function printQuote () {
 
 setInterval(function(){printQuote()}, 5000);
 
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
+// When a user clicks the Show another quote button the quote on the page will change
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
